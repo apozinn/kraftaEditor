afe376c81547490daf0bda3fcaf425275b09c7df
apozinn <samuelsoaresmonteiro12@gmail.com>
2025-01-29 20:00:49 -0300

    obsolete files removed

--- pages/settings/settings.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-class SettingsPage : public wxPanel {
-	wxPanel* title_panel;
-public:
-	SettingsPage(wxWindow* parent, wxWindowID ID);
-private:
-	void Paint(wxPaintEvent& event);
-};
