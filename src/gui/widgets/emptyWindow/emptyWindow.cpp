07f88b85f2768c5a0efc94f3bf370aa0a3c076d3
apozinn <samuelsoaresmonteiro12@gmail.com>
2026-05-25 16:31:15 -0300

    New logo added

--- src/gui/widgets/emptyWindow/emptyWindow.cpp
+++ src/gui/widgets/emptyWindow/emptyWindow.cpp
@@ -5,11 +5,4 @@ EmptyWindow::EmptyWindow(wxWindow* parent, wxWindowID ID) : wxPanel(parent, ID)
 {
 	auto background_color = Theme["secondary"].template get<std::string>();
 	SetBackgroundColour(wxColor(background_color));
-	sizer = new wxBoxSizer(wxVERTICAL);
-
-	wxImagePanel *logo = new wxImagePanel(this, assetsDir + "emptyWindowLogo.png", wxBITMAP_TYPE_PNG);
-	sizer->AddStretchSpacer();
-	sizer->Add(logo, 0, wxALL | wxALIGN_CENTER, 10);
-	sizer->AddStretchSpacer();
-	this->SetSizerAndFit(sizer);
 }
